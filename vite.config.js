7afc321abb295915afd4046c298cec7f7dd0e1c0
alanchan12354 <12345678+yourname@users.noreply.github.com>
2026-01-16 14:59:44 +0800

    feat: add gh-pages dependency and update base URL in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/https---github.com-alanchan12354-innocorn-ai-booth-preview-0116/',
 })
+
+  
